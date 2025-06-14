0c5596ea2721f1c5831ed80b445d802b3c5c2640
Mahi <mahex4@gmail.com>
2025-06-14 19:02:07 +0530

    Fixed docker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
-      },
-    ];
-  },
+  output: 'standalone',
+  experimental: {
+    optimizeCss: true,
+  }
 };
 
 export default nextConfig;
